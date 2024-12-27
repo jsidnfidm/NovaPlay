4ee143edd9a5d24ae49554afbf00e28b9836ebc7
Tsubaki Saito <129216571+jsidnfidm@users.noreply.github.com>
2024-12-27 07:36:32 -0300

    o Visual

---
+++ index.php
@@ -0,0 +1,288 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NovaPlay</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+     
+    <!--INICIO MENU DO SITE-->
+    <nav class="menu-site">
+       <!--INICIO MENU HAMBURGUER DO SITE-->
+       <div class="hamburguer-btn">
+        <span></span>
+        <span></span>
+        <span></span>
+        </div>
+        <!--FIM MENU HAMBURGUER DO SITE-->
+
+        <!--INICIO LOGO DO SITE-->
+        <img src="assets/img/logo.png" alt="Logo Tsubaki Saito" class="logo">
+        <!--FIM LOGO DO SITE-->
+        
+        <!--INICIO BUSCA DO SITE-->
+          <div class="busca-site">
+            <input type="text" class="busca-input" placeholder="O que você procura?">
+            <button class="busca-btn"><img src="assets/img/busca.png" alt=""></button>
+          </div>
+        <!--FIM BUSCA DO SITE-->
+
+
+         <!--INICIO OPÇÕES DO USUÁRIOS DO SITE-->
+          <div class="usuario-menu">
+              <img src="assets/img/video.PNG" class="icone" alt="">
+              <img src="assets/img/grid.PNG" class="icone" alt="">
+              <img src="assets/img/sininho.PNG" class="icone" alt="">
+              <!--INICIO PERFIL DO USUÁRIOS DO SITE-->
+              <div class="usuario-perfil">
+                   <img src="assets/img/perfil.png" alt="">
+              </div>
+              <!--FIM PERFIL DO USUÁRIOS DO SITE-->
+          </div>
+         <!--FIM OPÇÕES DO USUÁRIOS DO SITE-->
+
+    </nav>
+    <!--FIM MENU DO SITE-->
+
+     <!--INICIO BARRA LATERAL DO SITE-->
+     <div class="barra-lateral">
+        <a href="" class="links ativo"><img src="assets/img/inicio.png" alt="">Inicio</a>
+        <a href="" class="links"><img src="assets/img/explorer.png" alt="">Explorar</a>
+        <a href="" class="links"><img src="assets/img/shorts.png" alt="">Shorts</a>
+        <a href="" class="links"><img src="assets/img/inscritos.png" alt="">Inscrições</a>
+        <hr class="divisor">
+        <a href="" class="links"><img src="assets/img/biblioteca.png" alt="">Biblioteca</a>
+        <a href="" class="links"><img src="assets/img/historico.png" alt="">Histórico</a>
+        <a href="" class="links"><img src="assets/img/seus-videos.png" alt="">Seus vídeos</a>
+        <a href="" class="links"><img src="assets/img/veja-depois.png" alt="">Assistir mais tarde</a>
+        <a href="" class="links"><img src="assets/img/curtida.png" alt="">Vídeos que Gostei</a>
+        <a href="" class="links"><img src="assets/img/veja-mais.png" alt="">Mostrar mais</a>
+     </div>
+     <!--FIM BARRA LATERAL DO SITE-->
+
+     <!--INICIO FILTROS DO SITE-->
+       <div class="filtros">
+          <button class="opcoes-filtros ativo">Todos</button>
+          <button class="opcoes-filtros">Sheep</button>
+          <button class="opcoes-filtros">HTML</button>
+          <button class="opcoes-filtros">CSS</button>
+          <button class="opcoes-filtros">PHP8</button>
+          <button class="opcoes-filtros">JavaScript</button>
+          <button class="opcoes-filtros">Negócios Digitais</button>
+          <button class="opcoes-filtros">360 na sua vida</button>
+          <button class="opcoes-filtros">Tsubaki Saito</button>
+          <button class="opcoes-filtros">Viver do digital</button>
+       </div>
+     <!--FIM FILTROS DO SITE-->
+
+     <!--INICIO VIDEOS DO SITE-->
+      <div class="corpo-videos">
+
+
+         
+         <!--INICIO VIDEO UNICO DO SITE-->
+         <div class="video">
+             <img src="assets/img/capa1.jpg" class="miniatura" alt="">
+
+             <div class="conteudo-video">
+                <img src="assets/img/perfil.png" class="canal-icone" alt="">
+                 <div class="texto-video">
+                    <h4 class="titulo-video">Curso de negócios digitais</h4>
+                    <p class="nome-do-canal">Tsubaki Saito</p>
+                 </div>
+             </div>
+
+         </div>
+        <!--FIM VIDEO UNICO DO SITE-->
+
+
+         <!--INICIO VIDEO UNICO DO SITE-->
+         <div class="video">
+            <img src="assets/img/capa2.jpg" class="miniatura" alt="">
+
+            <div class="conteudo-video">
+               <img src="assets/img/perfil.png" class="canal-icone" alt="">
+                <div class="texto-video">
+                   <h4 class="titulo-video">Curso de negócios digitais</h4>
+                   <p class="nome-do-canal">Tsubaki Saito</p>
+                </div>
+            </div>
+
+        </div>
+       <!--FIM VIDEO UNICO DO SITE-->
+
+
+       
+
+
+        <!--INICIO VIDEO UNICO DO SITE-->
+        <div class="video">
+            <img src="assets/img/capa3.jpg" class="miniatura" alt="">
+
+            <div class="conteudo-video">
+               <img src="assets/img/perfil.png" class="canal-icone" alt="">
+                <div class="texto-video">
+                   <h4 class="titulo-video">Curso de negócios digitais</h4>
+                   <p class="nome-do-canal">Tsubaki Saito</p>
+                </div>
+            </div>
+
+        </div>
+       <!--FIM VIDEO UNICO DO SITE-->
+
+
+       <!--INICIO VIDEO UNICO DO SITE-->
+       <div class="video">
+        <img src="assets/img/capa4.jpg" class="miniatura" alt="">
+
+        <div class="conteudo-video">
+           <img src="assets/img/perfil.png" class="canal-icone" alt="">
+            <div class="texto-video">
+               <h4 class="titulo-video">Curso de negócios digitais</h4>
+               <p class="nome-do-canal">Tsubaki Saito</p>
+            </div>
+        </div>
+
+    </div>
+   <!--FIM VIDEO UNICO DO SITE-->
+
+
+   <!--INICIO VIDEO UNICO DO SITE-->
+   <div class="video">
+    <img src="assets/img/capa5.jpg" class="miniatura" alt="">
+
+    <div class="conteudo-video">
+       <img src="assets/img/perfil.png" class="canal-icone" alt="">
+        <div class="texto-video">
+           <h4 class="titulo-video">Curso de negócios digitais</h4>
+           <p class="nome-do-canal">Tsubaki Saito</p>
+        </div>
+    </div>
+
+</div>
+<!--FIM VIDEO UNICO DO SITE-->
+
+
+ <!--INICIO VIDEO UNICO DO SITE-->
+ <div class="video">
+    <img src="assets/img/capa6.jpg" class="miniatura" alt="">
+
+    <div class="conteudo-video">
+       <img src="assets/img/perfil.png" class="canal-icone" alt="">
+        <div class="texto-video">
+           <h4 class="titulo-video">Curso de negócios digitais</h4>
+           <p class="nome-do-canal">Tsubaki Saito</p>
+        </div>
+    </div>
+
+</div>
+<!--FIM VIDEO UNICO DO SITE-->
+
+
+<!--INICIO VIDEO UNICO DO SITE-->
+<div class="video">
+    <img src="assets/img/capa7.jpg" class="miniatura" alt="">
+
+    <div class="conteudo-video">
+       <img src="assets/img/perfil.png" class="canal-icone" alt="">
+        <div class="texto-video">
+           <h4 class="titulo-video">Curso de negócios digitais</h4>
+           <p class="nome-do-canal">Tsubaki Saito</p>
+        </div>
+    </div>
+
+</div>
+<!--FIM VIDEO UNICO DO SITE-->
+
+
+<!--INICIO VIDEO UNICO DO SITE-->
+<div class="video">
+    <img src="assets/img/capa8.jpg" class="miniatura" alt="">
+
+    <div class="conteudo-video">
+       <img src="assets/img/perfil.png" class="canal-icone" alt="">
+        <div class="texto-video">
+           <h4 class="titulo-video">Curso de negócios digitais</h4>
+           <p class="nome-do-canal">Tsubaki Saito</p>
+        </div>
+    </div>
+
+</div>
+<!--FIM VIDEO UNICO DO SITE-->
+
+
+<!--INICIO VIDEO UNICO DO SITE-->
+<div class="video">
+    <img src="assets/img/capa9.jpg" class="miniatura" alt="">
+
+    <div class="conteudo-video">
+       <img src="assets/img/perfil.png" class="canal-icone" alt="">
+        <div class="texto-video">
+           <h4 class="titulo-video">Curso de negócios digitais</h4>
+           <p class="nome-do-canal">Tsubaki Saito</p>
+        </div>
+    </div>
+
+</div>
+<!--FIM VIDEO UNICO DO SITE-->
+
+
+<!--INICIO VIDEO UNICO DO SITE-->
+<div class="video">
+    <img src="assets/img/capa10.jpg" class="miniatura" alt="">
+
+    <div class="conteudo-video">
+       <img src="assets/img/perfil.png" class="canal-icone" alt="">
+        <div class="texto-video">
+           <h4 class="titulo-video">Curso de negócios digitais</h4>
+           <p class="nome-do-canal">Tsubaki Saito</p>
+        </div>
+    </div>
+
+</div>
+<!--FIM VIDEO UNICO DO SITE-->
+
+
+<!--INICIO VIDEO UNICO DO SITE-->
+<div class="video">
+    <img src="assets/img/capa11.jpg" class="miniatura" alt="">
+
+    <div class="conteudo-video">
+       <img src="assets/img/perfil.png" class="canal-icone" alt="">
+        <div class="texto-video">
+           <h4 class="titulo-video">Curso de negócios digitais</h4>
+           <p class="nome-do-canal">Tsubaki Saito</p>
+        </div>
+    </div>
+
+</div>
+<!--FIM VIDEO UNICO DO SITE-->
+
+
+<!--INICIO VIDEO UNICO DO SITE-->
+<div class="video">
+    <img src="assets/img/capa12.jpg" class="miniatura" alt="">
+
+    <div class="conteudo-video">
+       <img src="assets/img/perfil.png" class="canal-icone" alt="">
+        <div class="texto-video">
+           <h4 class="titulo-video">Curso de negócios digitais</h4>
+           <p class="nome-do-canal">Tsubaki Saito</p>
+        </div>
+    </div>
+
+</div>
+<!--FIM VIDEO UNICO DO SITE-->
+
+
+      </div>
+     <!--FIM VIDEOS DO SITE-->
+    
+    
+    <script src="assets/js/app.js"></script>
+    
+</body>
+</html>
